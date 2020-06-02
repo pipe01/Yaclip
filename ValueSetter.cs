f1a65da8c2feab434b4ec1a3bf72265cbe88009f
Felipe Martinez <pipernene@gmail.com>
2020-06-02 13:39:43 +0200

    Parse Version values

--- ValueSetter.cs
+++ ValueSetter.cs
@@ -20,27 +20,34 @@ namespace LogicCommandLineParser
 
         public static bool TryParseValue(string str, Type type, [MaybeNullWhen(false)]out object value)
         {
-            value = Type.GetTypeCode(type) switch
+            if (type == typeof(Version))
             {
-                TypeCode.String => str,
-                TypeCode.Char => char.Parse(str),
-                TypeCode.Byte => byte.Parse(str),
-                TypeCode.SByte => sbyte.Parse(str),
-                TypeCode.Int16 => short.Parse(str),
-                TypeCode.UInt16 => ushort.Parse(str),
-                TypeCode.Int32 => int.Parse(str),
-                TypeCode.UInt32 => uint.Parse(str),
-                TypeCode.Int64 => long.Parse(str),
-                TypeCode.UInt64 => ulong.Parse(str),
-                TypeCode.Single => float.Parse(str, CultureInfo.InvariantCulture),
-                TypeCode.Double => double.Parse(str, CultureInfo.InvariantCulture),
-                TypeCode.Decimal => decimal.Parse(str, CultureInfo.InvariantCulture),
-                TypeCode.Boolean =>
-                    str.Equals("yes", StringComparison.InvariantCultureIgnoreCase) ? true :
-                    str.Equals("no", StringComparison.InvariantCultureIgnoreCase) ? false :
-                    bool.Parse(str),
-                _ => null
-            };
+                value = Version.Parse(str);
+            }
+            else
+            {
+                value = Type.GetTypeCode(type) switch
+                {
+                    TypeCode.String => str,
+                    TypeCode.Char => char.Parse(str),
+                    TypeCode.Byte => byte.Parse(str),
+                    TypeCode.SByte => sbyte.Parse(str),
+                    TypeCode.Int16 => short.Parse(str),
+                    TypeCode.UInt16 => ushort.Parse(str),
+                    TypeCode.Int32 => int.Parse(str),
+                    TypeCode.UInt32 => uint.Parse(str),
+                    TypeCode.Int64 => long.Parse(str),
+                    TypeCode.UInt64 => ulong.Parse(str),
+                    TypeCode.Single => float.Parse(str, CultureInfo.InvariantCulture),
+                    TypeCode.Double => double.Parse(str, CultureInfo.InvariantCulture),
+                    TypeCode.Decimal => decimal.Parse(str, CultureInfo.InvariantCulture),
+                    TypeCode.Boolean =>
+                        str.Equals("yes", StringComparison.InvariantCultureIgnoreCase) ? true :
+                        str.Equals("no", StringComparison.InvariantCultureIgnoreCase) ? false :
+                        bool.Parse(str),
+                    _ => null
+                };
+            }
 
             return value != null;
         }
